aaf23470ab5fb2e9185ac26e4476c41012337531
Daniel López Pérez <dlopezp.dev@gmail.com>
2020-01-19 21:32:18 +0100

    First approach to DiscordBotAdmin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DiscordBotModule } from './discord-bot/discord-bot.module';
 
 @Module({
-  imports: [],
+  imports: [DiscordBotModule],
   controllers: [AppController],
   providers: [AppService],
 })
